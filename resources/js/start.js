70dce17b7503ce350efc58e6cfdc7c81fc566441
fddeath <254075058+fddeath@users.noreply.github.com>
2026-03-23 14:35:46 +1000

    add: form

--- resources/js/start.js
+++ resources/js/start.js
@@ -2,15 +2,21 @@ import data from '../../modules/start_config.json' with { type: 'json' };
 let current_step = 0;
 
 document.addEventListener('click', (e) => {
-    const title = document.querySelector('.title');
-    const description = document.querySelector('.description');
-    const button_space = document.querySelector('.button_space');
-    const button = document.querySelector('.sign_button');
-    const button_back = document.querySelector('.sign_button_back');
+    const container = document.querySelector('.container');
+
+    const form_info = container.querySelector('.form_info');
+    const inputs = container.querySelectorAll('.step');
+
+    const sign = container.querySelector('.sign');
+    const title = container.querySelector('.title');
+    const description = container.querySelector('.description');
+    const button_space = container.querySelector('.button_space');
+    const button = container.querySelector('.sign_button');
+    const button_back = container.querySelector('.sign_button_back');
 
     // Button - next
 
-    if (button && e.target == button && current_step < data['step_length']) {
+    if (button && e.target === button && current_step < data['steps_info'].length) {
         let isset_step = false;
 
         button.className.split(' ').forEach(cls => {
@@ -42,14 +48,6 @@ document.addEventListener('click', (e) => {
 
     // Button - back
 
-    if (button_back && current_step <= 2) {
-        button_back.remove();
-    }
-
-    if (button_space && current_step > 1) {
-        if (!button_back) button_space.insertAdjacentHTML('afterbegin', `<button class="button sign_button_back">Назад</button>`);
-    }
-
     if (button_back && e.target == button_back) {
         current_step--;
         if (current_step > 0) {
@@ -66,6 +64,22 @@ document.addEventListener('click', (e) => {
         }
     }
 
+    // innerText in buttons
+
+    if (button && current_step == data['steps_info'].length) {
+        button.innerText = 'Готово';
+    } else if (button && current_step != data['steps_info'].length) {
+        button.innerText = 'Далее';
+    }
+
+    if (button_back && current_step < 2) {
+        button_back.remove();
+    }
+
+    if (button_space && current_step > 1) {
+        if (!button_back) button_space.insertAdjacentHTML('afterbegin', `<button class="button sign_button_back" type="button">Назад</button>`);
+    }
+
     // Sing
 
     if (title && description && current_step > 0) {
@@ -76,7 +90,32 @@ document.addEventListener('click', (e) => {
             }
         })
         title.innerText = 'Шаг ' + current_step;
-        if (text) description.innerText = text;
+        if (text) description.innerHTML = text;
         else description.innerText = 'Null';
+    }
+
+    // Form
+
+    if (form_info && current_step > 0) {
+        form_info.classList.remove('dNone');
+
+        inputs.forEach(element => {
+            const title = element.querySelector('.input_title');
+
+            if (element.id == 'step_' + current_step) {
+                element.classList.remove('dNone');
+
+                let issetTitle = false;
+                data['steps_info'].forEach(step => {
+                    if (current_step == step['id']) {
+                        issetTitle = step['title'];
+                    }
+                });
+                if (issetTitle) title.innerText = issetTitle;
+                else title.innerText = "Null";
+            } else {
+                element.classList.add('dNone');
+            }
+        });
     }
 });
--- resources/view/start.php
+++ resources/view/start.php
@@ -7,22 +7,94 @@
 
     <link rel="stylesheet" href="resources/css/vars.css">
     <link rel="stylesheet" href="resources/css/main.css">
+    <link rel="stylesheet" href="resources/css/start.css">
     <link rel="shortcut icon" href=" " type="image/x-icon">
 
     <script type="module" src="resources/js/start.js" defer></script>
     <script src="resources/js/dropdown.js" defer></script>
 </head>
     <body>
+        <form class="container" action="" method="post">
 
-        <div class="sign">
-            <h2 class="title">CloudSpace | Resume</h2>
-
-            <p class="description">Для создания резюме нажми <b>"Начать"</b></p>
-
-            <div class="button_space">
-                <button class="button sign_button">Начать</button>
+            <div class="form_info dNone">
+                <div id="step_1" class="step dNone">
+                    <h3 class="input_title">Title</h3>
+                    <div class="input_container">
+                        <input
+                            type="text" 
+                            name="name"
+                            placeholder="Имя"
+                        >
+                    </div>
+                    <div class="input_container">
+                        <input
+                            class="dNone"
+                            type="file" 
+                            name="img"
+                            id="img"
+                        >
+                        <label for="img" class="input_file">Фото профиля</label>
+                    </div>
+                    <div class="input_container">
+                        <textarea
+                            name="about_me"
+                            cols="30"
+                            placeholder="О себе"
+                        ></textarea>
+                    </div>
+                </div>
+                <div id="step_2" class="step dNone">
+                    <h3 class="input_title">Title</h3>
+                    <div class="input_container">
+                        <input
+                            type="text" 
+                            name="experience"
+                            placeholder="Опыт работы"
+                        >
+                    </div>
+                    <button type="button" class="input_button">Add</button>
+                </div>
+                <div id="step_3" class="step dNone">
+                    <h3 class="input_title">Title</h3>
+                    <div class="input_container">
+                        <input
+                            type="text" 
+                            name="social"
+                            placeholder="URL"
+                        >
+                    </div>
+                    <button type="button" class="input_button">Add</button>
+                </div>
+                <div id="step_4" class="step dNone">
+                    <h3 class="input_title">Title</h3>
+                    <div class="input_container">
+                        <input
+                            type="text" 
+                            name="projects"
+                            placeholder="URL"
+                        >
+                    </div>
+                    <div class="input_container">
+                        <textarea
+                            name="about_me"
+                            cols="30"
+                            placeholder="Описание проделанных работ"
+                        ></textarea>
+                    </div>
+                    <button type="button" class="input_button">Add</button>
+                </div>
             </div>
-        </div>
 
+            <div class="sign">
+                <h2 class="title">CloudSpace | Resume</h2>
+
+                <p class="description">Для создания резюме нажми <b>"Начать"</b></p>
+
+                <div class="button_space">
+                    <button type="button" class="button sign_button">Начать</button>
+                </div>
+            </div>
+
+        </form>
     </body>
 </html>
